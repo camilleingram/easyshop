0f67d75f9edcd4a593dd5222fcace91ce3d5a5a4
Camille Ingram <camille_d_ingram@live.com>
2025-06-24 16:56:01 -0400

    Implement GET all categories

--- easyshop-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ easyshop-api/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,23 +5,46 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 {
+    private final DataSource dataSource;
+
     public MySqlCategoryDao(DataSource dataSource)
     {
         super(dataSource);
+        this.dataSource = dataSource;
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
+    public List<Category> getAllCategories() {
         // get all categories
-        return null;
+        List<Category> categories = new ArrayList<>();
+
+        String query = "SELECT * FROM categories";
+
+        try(Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet row = preparedStatement.executeQuery();
+
+            while(row.next()) {
+                 Category category = mapRow(row);
+                 categories.add(category);
+            }
+
+        }catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return categories;
     }
 
     @Override
