ca8083d21fb9a803a6bad07f21c8f11b94904be9
Camille Ingram <camille_d_ingram@live.com>
2025-06-25 17:57:44 -0400

    Resolve add to category

--- easyshop-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ easyshop-api/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,48 +1,85 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
+import java.sql.SQLException;
 
 // convert this class to a REST controller
 // only logged in users should have access to these actions
-public class ShoppingCartController
-{
+@RestController
+@RequestMapping("cart")
+@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+public class ShoppingCartController {
+
     // a shopping cart requires
     private ShoppingCartDao shoppingCartDao;
     private UserDao userDao;
     private ProductDao productDao;
+    private ShoppingCart shoppingCart;
 
-
+    @Autowired
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
-            // get the currently logged in username
+    @GetMapping
+    public ShoppingCart getCart(Principal principal) {
+        try {
             String userName = principal.getName();
-            // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
+            ShoppingCart cart = shoppingCartDao.getByUserId(userId);
 
-            // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
+            if(cart == null) {
+                // Return an empty cart instead of null
+                cart = new ShoppingCart();
+            }
+
+            return cart;
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("products/{id}")
+    public ShoppingCart addToShoppingCart(@PathVariable int id, Principal principal) {
+
+        try {
+            String username = principal.getName();
+            User user = userDao.getByUserName(username);
+            int userId = user.getId();
+
+            Product product = productDao.getById(id);
+            if(product == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+            }
+
+            shoppingCartDao.addToCart(id, userId);
+            return shoppingCartDao.getByUserId(userId);
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to add item to cart");
+        }
+
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
---
+++ easyshop-api/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,103 @@
+package org.yearup.data.mysql;
+
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        String query = "SELECT shopping_cart.user_id, shopping_cart.product_id, shopping_cart.quantity, products.product_id," +
+                "products.name, products.price, products.category_id, products.description, products.color,products.stock," +
+                "products.image_url, products.featured " +
+                "FROM shopping_cart " +
+                "JOIN products ON shopping_cart.product_id = products.product_id " +
+                "WHERE shopping_cart.user_id = ?";
+
+        try(Connection connection = getConnection()) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+
+
+            ResultSet row = preparedStatement.executeQuery();
+
+            ShoppingCart cart = new ShoppingCart();
+
+            while(row.next()) {
+                Product product = mapProduct(row);
+                int quantity = row.getInt("quantity");
+
+                ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+                shoppingCartItem.setProduct(product);
+                shoppingCartItem.setQuantity(quantity);
+                shoppingCartItem.setDiscountPercent(BigDecimal.ZERO);
+                cart.add(shoppingCartItem);
+
+            }
+
+            return cart;
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void addToCart(int productId, int userId) {
+        String query = "INSERT INTO shopping_cart (user_id, product_id, quantity) " +
+                "VALUES(?, ?, 1) " +
+                "ON DUPLICATE KEY UPDATE quantity = quantity + 1";
+
+        try(Connection connection = getConnection()) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, userId);
+            preparedStatement.setInt(2, productId);
+
+            preparedStatement.executeUpdate();
+
+
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    private Product mapProduct(ResultSet row) throws SQLException {
+
+        int productId = row.getInt("product_id");
+        String name = row.getString("name");
+        BigDecimal price = row.getBigDecimal("price");
+        int categoryId = row.getInt("category_id");
+        String description = row.getString("description");
+        String color = row.getString("color");
+        String imageUrl = row.getString("image_url");
+        int stock = row.getInt("stock");
+        boolean featured = row.getBoolean("featured");
+
+
+        Product addedProduct = new Product(productId, name, price, categoryId, description, color, stock, featured, imageUrl);
+
+        return addedProduct;
+    }
+}
